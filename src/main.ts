a316144fa8e867a1eb9f2bd964ca652b1447a0f3
mohamed saber <m.saber@bedoeg.com>
2026-09-24 11:31:05 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
